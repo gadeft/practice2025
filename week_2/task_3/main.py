0aa4f3af8640e07e5f0d77cb48835ce1f550f751
Dima <dimaisay1109@gmail.com>
2025-06-01 22:50:28 +0300

    3-rd tash fixed

--- week_2/task_3/main.py
+++ week_2/task_3/main.py
@@ -2,11 +2,11 @@ from datetime import datetime
 import sys
 
 class Logger:
-    def __init__(self, out_stream, formatter):
+    def __init__(self, out_stream = sys.stdout, formatter = "%Y.%m.%d %H:%M:%S"):
         self._out_stream = out_stream
         self._formatter = formatter
 
     def log(self, message):
         now = datetime.now()
-        print("[%s] %s" % (now.strftime(self._formatter), message))
-
+        now = now.strftime(self._formatter)
+        print("[%s] %s" % (now, message), file = self._out_stream)
